83da15a12c1ec3af59558da10b8881b28e5a9510
MalkingMan <arrayalkhozini@gmail.com>
2025-11-07 13:05:52 +0700

    Buat AppNavigation Composable

--- app/src/main/java/com/example/navigation_layout/AppNavigation.kt
+++ app/src/main/java/com/example/navigation_layout/AppNavigation.kt
@@ -4,3 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "welcome") {
+        composable("welcome") {
+            WelcomeScreen(onNavigateToForm = { navController.navigate("form") })
+        }
+        composable("form") {
+            FormScreen(onNavigateBack = { navController.popBackStack() })
+        }
+    }
+}
