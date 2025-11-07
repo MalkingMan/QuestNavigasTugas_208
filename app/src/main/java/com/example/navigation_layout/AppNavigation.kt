4a83a11ebd7304b98eba4915ab5642b3ae9010ec
MalkingMan <arrayalkhozini@gmail.com>
2025-11-07 13:04:59 +0700

    Tambahkan impor NavHost

--- app/src/main/java/com/example/navigation_layout/AppNavigation.kt
+++ app/src/main/java/com/example/navigation_layout/AppNavigation.kt
@@ -1,1 +1,6 @@
 package com.example.navigation_layout
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
